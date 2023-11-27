de9f0f95db188160c6038b7f58ec4e44c9c63980
LimJongTak <39991534+LimJongTak@users.noreply.github.com>
2023-11-27 10:56:38 +0900

    Update customer_service.py

--- customer_service.py
+++ customer_service.py
@@ -3,5 +3,5 @@ from app import app
 
 @app.route('/customer_management')
 def customer_management():
-    # 아래 코드에 고객 관리 페이지의 로직을 추가 가능.
+    # 아래 코드에 고객 관리 페이지의 로직을 추가 가능
     return render_template('customer_management.html')
