0ee9d1de7464941a8332cccdeb2b883b088d4e4a
LimJongTak <39991534+LimJongTak@users.noreply.github.com>
2023-11-27 10:56:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
 
-# 아래에 추가되는 라우트와 뷰 함수들을 가져오기 위해 각 파일을 import
+# 아래에 추가되는 라우트와 뷰 함수를 가져오기 위해 각 파일을 import
 from login import *
 from signup import *
 from main import *
